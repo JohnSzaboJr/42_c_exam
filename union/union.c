fce2633565035c59d2bfffd057e5b94a61e7d201
Janos SZABO <jszabo@e2r11p7.42.fr>
2017-11-08 17:58:02 +0100

    initial commit

---
+++ union/union.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   union.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jszabo <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/11/08 17:37:36 by jszabo            #+#    #+#             */
+/*   Updated: 2017/11/08 17:42:39 by jszabo           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdio.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_union(char *str1, char *str2)
+{
+
+}
+
+int		main(int argc, char **argv)
+{
+	if (argc == 3)
+		ft_union(argv[1], argv[2]);
+	ft_putchar('\n');
+	return (0);
+}
